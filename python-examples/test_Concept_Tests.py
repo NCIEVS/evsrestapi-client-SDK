c157ecaa1c31c297164e562f9f698805966529ec
Peter Van Ausdeln <pvanaus@ucdavis.edu>
2020-06-30 16:00:56 -0700

    fix parts of readme and add more testing

--- python-examples/test_Concept_Tests.py
+++ python-examples/test_Concept_Tests.py
@@ -21,16 +21,30 @@ def test_get_minimal_concept_by_code(): # Return all loaded terminologies curren
     assert response.status_code == requests.codes.ok;
     logging.info(response.text);
 
-def test_get_minimal_concept_list_by_code(): # Return all loaded terminologies currently hosted by the API.
+def test_get_minimal_concept_list_by_code(): # Return concept objects with minimal information for a specified list of codes.
     logging.info("test_Concept_Tests.py: Get minimal concepts by list - [C3224, C3910]");
     logging.info("url = " + localUrl + "/concept/ncit?include=minimal&list=C3224,C3910");
     response = requests.get(localUrl + "/concept/ncit?include=minimal&list=C3224,C3910");
     assert response.status_code == requests.codes.ok;
     logging.info(response.text);
 
-def test_get_summary_concept_by_code(): # Return all loaded terminologies currently hosted by the API.
+def test_get_summary_concept_by_code(): # Return concept object with summary information for a specified code.
     logging.info("test_Concept_Tests.py: Get summary concepts by code - C3224");
-    logging.info("url = " + localUrl + "/concept/ncit?include=summary&list=C3224,C3910");
-    response = requests.get(localUrl + "/concept/ncit?include=summary&list=C3224,C3910");
+    logging.info("url = " + localUrl + "/concept/ncit/C3224?include=summary");
+    response = requests.get(localUrl + "/concept/ncit/C3224?include=summary&list=C3224");
+    assert response.status_code == requests.codes.ok;
+    logging.info(response.text);
+
+def test_get_full_concept_by_code(): # Return concept object with full information for a specified code.
+    logging.info("test_Concept_Tests.py: Get full concepts by code - C3224");
+    logging.info("url = " + localUrl + "/concept/ncit/C3224?include=full");
+    response = requests.get(localUrl + "/concept/ncit/C3224?include=full");
+    assert response.status_code == requests.codes.ok;
+    logging.info(response.text);
+
+def test_get_custom_concept_by_code(): # Return custom concept information for a given terminology and code.
+    logging.info("test_Concept_Tests.py: Get custom concepts by code - C3224");
+    logging.info("url = " + localUrl + "/concept/ncit/C3224?include=synonyms,children,maps,inverseAssociations");
+    response = requests.get(localUrl + "/concept/ncit/C3224?include=synonyms,children,maps,inverseAssociations");
     assert response.status_code == requests.codes.ok;
     logging.info(response.text);
