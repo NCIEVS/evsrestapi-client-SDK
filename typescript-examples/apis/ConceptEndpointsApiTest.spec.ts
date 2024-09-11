54809055a57b15694cdf0889aa64ad04880efdf6
peter-va <pvanaus@ucdavis.edu>
2024-09-11 00:49:42 -0700

    other changes

--- typescript-examples/apis/ConceptEndpointsApiTest.spec.ts
+++ typescript-examples/apis/ConceptEndpointsApiTest.spec.ts
@@ -1,44 +1,46 @@
-import {AssociationEntryResultList} from '../models/AssociationEntryResultList'
-import {Association} from '../models/Association'
-import {Concept} from '../models/Concept'
-import {DisjointWith} from "../models/DisjointWith";
-import {HierarchyNode} from "../models/HierarchyNode";
-import {Role} from "../models/Role";
-import {ConceptMap} from "../models/ConceptMap";
-import { ConceptEndpointsApiRequestFactory, ConceptEndpointsApiResponseProcessor } from '../apis/ConceptEndpointsApi';
-import { PromiseConceptEndpointsApi } from '../types/PromiseAPI';
+import { ConceptEndpointsApi } from "..";
+import { createConfiguration } from "../configuration";
+import { AssociationEntryResultList } from "../models/AssociationEntryResultList";
 
-import {Logger} from 'tslog';
-import {Configuration } from '..';
-
+import { Logger } from "tslog";
 
 const logger: Logger<string> = new Logger();
 
-const configuration = new Configuration()
+const configuration = createConfiguration();
 
-describe('TestConceptEndpointsApi', () => {
-    const terminology: string = "ncit";
-    const code: string = "C3224";
-    const conceptApi: ConceptEndpointsApi = new ConceptEndpointsApi();
+describe("TestConceptEndpointsApi", () => {
+  const terminology: string = "ncit";
+  const code: string = "C3224";
+  const conceptApi: ConceptEndpointsApi = new ConceptEndpointsApi(
+    configuration
+  );
 
-    test('test_get_association_entries', async () => {
-        const fromRecord: number = 0;
-        const pageSize: number = 100;
-        const expectedRelatedName: string = 'Naluzotan Hydrochloride';
-        let containsExpectedText = false;
+  test("test_get_association_entries", async () => {
+    const fromRecord: number = 0;
+    const pageSize: number = 100;
+    const expectedRelatedName: string = "Naluzotan Hydrochloride";
+    let containsExpectedText = false;
 
-        const response: AssociationEntryResultList = await conceptApi.getAssociationEntries(terminology, code, fromRecord, pageSize, null);
-        for (const entry of response.getAssociationEntries()) {
-            expect(entry.getRelatedName()).not.toBeNull();
-            if (entry.getRelatedName().includes(expectedRelatedName)) {
-                containsExpectedText = true;
-                break;
-            }
-        }
+    const response: AssociationEntryResultList =
+      await conceptApi.getAssociationEntries(
+        terminology,
+        code,
+        fromRecord,
+        pageSize,
+        undefined
+      );
 
-        expect(response).not.toBeNull();
-        expect(response.getAssociationEntries()).not.toBeNull();
-        expect(containsExpectedText).toBe(true);
+    expect(response).not.toBeNull();
 
-    });
+    expect(response?.associationEntries).not.toBeNull();
+    expect(response?.associationEntries).not.toBeUndefined();
+    for (const entry of response?.associationEntries) {
+      expect(entry?.relatedName).not.toBeNull();
+      if (entry?.relatedName?.includes(expectedRelatedName)) {
+        containsExpectedText = true;
+        break;
+      }
+    }
+    expect(containsExpectedText).toBe(true);
+  });
 });
