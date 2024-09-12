ba66a3df10477549e5926dd67e0264456b11f25f
peter-va <pvanaus@ucdavis.edu>
2024-09-12 02:06:29 -0700

    subset tests

--- go-examples/test/api_subset_endpoints_test.go
+++ go-examples/test/api_subset_endpoints_test.go
@@ -11,59 +11,82 @@ package evs
 
 import (
 	"context"
+	"testing"
+
+	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"testing"
-	openapiclient "github.com/GIT_USER_ID/GIT_REPO_ID"
 )
 
 func Test_evs_SubsetEndpointsAPIService(t *testing.T) {
 
 	configuration := openapiclient.NewConfiguration()
 	apiClient := openapiclient.NewAPIClient(configuration)
+	terminology := "ncit"
 
 	t.Run("Test SubsetEndpointsAPIService GetSubset", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		// ARRANGE
+		code := "C116978"
+		include := "minimal,summary"
+		expectedName := "CTRP Agent Terminology"
 
-		var terminology string
-		var code string
+		// ACT
+		resp, httpRes, err := apiClient.SubsetEndpointsAPI.GetSubset(context.Background(), terminology, code).
+			Include(include).Execute()
 
-		resp, httpRes, err := apiClient.SubsetEndpointsAPI.GetSubset(context.Background(), terminology, code).Execute()
-
+		// ASSERT
 		require.Nil(t, err)
 		require.NotNil(t, resp)
 		assert.Equal(t, 200, httpRes.StatusCode)
-
+		assert.Equal(t, expectedName, resp.GetName(), "FAIL: expected name doesn't match actual")
 	})
 
 	t.Run("Test SubsetEndpointsAPIService GetSubsetMembers", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		// ARRANGE
+		code := "C157225"
+		fromRecord := "0"
+		pageSize := "25"
+		include := "minimal"
+		expectedCode := "C16255"
+		expectedName := "Acetylation"
+		containsExpectedValues := false
 
-		var terminology string
-		var code string
+		// ACT
+		resp, httpRes, err := apiClient.SubsetEndpointsAPI.GetSubsetMembers(context.Background(), terminology, code).
+			FromRecord(fromRecord).PageSize(pageSize).Include(include).Execute()
 
-		resp, httpRes, err := apiClient.SubsetEndpointsAPI.GetSubsetMembers(context.Background(), terminology, code).Execute()
-
+		// ASSERT
 		require.Nil(t, err)
 		require.NotNil(t, resp)
 		assert.Equal(t, 200, httpRes.StatusCode)
 
+		for _, subset := range resp {
+			assert.NotNil(t, subset.GetName())
+			assert.NotNil(t, subset.GetCode())
+			if subset.GetName() == expectedName && subset.GetCode() == expectedCode {
+				containsExpectedValues = true
+				break
+			}
+		}
+
+		assert.True(t, containsExpectedValues, "FAIL: expected subset name and code not found")
 	})
 
 	t.Run("Test SubsetEndpointsAPIService GetSubsets", func(t *testing.T) {
 
-		t.Skip("skip test")  // remove to run test
+		// ARRANGE
+		include := "minimal"
 
-		var terminology string
+		// ACT
+		resp, httpRes, err := apiClient.SubsetEndpointsAPI.GetSubsets(context.Background(), terminology).
+			Include(include).Execute()
 
-		resp, httpRes, err := apiClient.SubsetEndpointsAPI.GetSubsets(context.Background(), terminology).Execute()
-
+		// ASSERT
 		require.Nil(t, err)
 		require.NotNil(t, resp)
 		assert.Equal(t, 200, httpRes.StatusCode)
-
 	})
 
 }
