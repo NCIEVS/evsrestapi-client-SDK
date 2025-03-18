d4eed17c3bf3d7024227f993ac2b620053458188
Peter Van Ausdeln <pvanausdeln@westcoastinformatics.com>
2025-03-18 15:37:59 -0700

    Pva/fix go tests (#33)
    fix go tests

--- go-examples/history_test.go
+++ go-examples/history_test.go
@@ -1,28 +1,29 @@
-package tests
+package evs_test
 
 import (
 	"fmt"
 	"testing"
+	"evs"
 )
 
 // Get the replacement concepts for a specified inactive concept code.
 func TestGetInactiveReplacementCode(t *testing.T) {
-	var used_url = ValidateUrlFile()
+	var used_url = evs.ValidateUrlFile()
 
 	fmt.Println("history_test.go: Get inactive replacement code")
 	fmt.Println(string(used_url) + "/history/ncit/replacements?list=C12658\n")
 	var url = string(used_url) + "/history/ncit/replacements?list=C12658"
 
-	ProcessData(url)
+	evs.ProcessData(url)
 }
 
 // Get the replacement concepts for a specified list of inactive concept codes.
 func TestGetInactiveReplacementCodes(t *testing.T) {
-	var used_url = ValidateUrlFile()
+	var used_url = evs.ValidateUrlFile()
 
 	fmt.Println("history_test.go: Get list of inactive replacement codes")
 	fmt.Println(string(used_url) + "/history/ncit/replacements?list=C12658,C13320\n")
 	var url = string(used_url) + "/history/ncit/replacements?list=C12658,C13320"
 
-	ProcessData(url)
+	evs.ProcessData(url)
 }
